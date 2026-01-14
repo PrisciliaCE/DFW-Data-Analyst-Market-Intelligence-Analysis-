3815cf374efe8a89ac386e5f35d6cbd85fef947b
PrisciliaCE <eypriscilia517@gmail.com>
2026-01-14 17:52:52 -0600

    Created Database

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,7 @@
+/* 
+   Analysis: Highest-Paying Remote Data Analyst & Data Engineer Roles
+   - Extract the top 10 highest-paying remote roles for Data Analysts and Data Engineers.
+   - Filter out postings without salary data to focus on meaningful compensation insights.
+   - Why? Objective: Identify lucrative opportunities and surface key skills that drive higher pay in the data analytics and engineering job market.
+*/
+
